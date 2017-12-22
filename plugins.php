5955a86d9b807052386f48583407148f297edc2c
Don Mac <don_macadat@dlsu.edu.ph>
2017-12-23 01:39:38 +0800

    fixing includes

--- plugins.php
+++ plugins.php
@@ -14,23 +14,18 @@
 <link rel="stylesheet" href="resources/includes/css/jhtml/jqueryui/ui-lightness/jquery-ui-1.7.2.custom.css">
   <!-- Bootstrap Core CSS -->
 
-    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
-    <!-- MetisMenu CSS -->
-    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="dist/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="https://cdn.bootcss.com/startbootstrap-sb-admin-2/3.3.7+1/css/sb-admin-2.min.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <!-- Multi select-->
-    <link href="dist/css/bootstrap-multiselect.css" rel="stylesheet" type="text/css">
     <!-- jQuery -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
 
     <!--Google Fonts-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
