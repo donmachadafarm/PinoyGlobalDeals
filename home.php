18c8f7f1a52ee896812af894d7c3d9f21988863a
sonnylao <34619146+sonnylao@users.noreply.github.com>
2017-12-24 16:04:25 +0800

    Add files via upload

--- home.php
+++ home.php
@@ -6,116 +6,48 @@
     <meta name="google-site-verification" content="ypZaVNwO_O8FrwuhdtzDqzLOfgWvLZ2AA9vmSbyCTOQ" />
     <meta name="propeller" content="ed552b5a8ac60625ae31beb6db5d9e45" />
     <title>Pinoy Global Deals</title>
+    <link rel="Shortcut Icon" type="image/ico" href="img/favicon.ico">
     <meta name="description" content="">
-    <?php include 'plugins.php' ?>
-</head>
+
+
 <body>
-    <?php include 'header.php' ?>
-    <!-- <input type="hidden" value="logout.php" id="logout-url"/>
-    <div id="loader" style="display: block;">
-          <div id="loader-img">
-              <div class="sk-folding-cube">
-                <div class="sk-cube1 sk-cube"></div>
-                <div class="sk-cube2 sk-cube"></div>
-                <div class="sk-cube4 sk-cube"></div>
-                <div class="sk-cube3 sk-cube"></div>
-              </div>
-          </div>
+  <?php
+  include 'nav_login.php'
+  ?>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" style="width:100%">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="carousel/img1.jpg" alt="img1" style="width:100%">
     </div>
-    <div id="tams-bg">
-        <div id="tamodal">
-            <div class="clearfix">
-                <div id="tams-content"></div>
-                <input type="hidden" id="msg-func" value="">
-                <input type="hidden" id="msg-loc" value="">
-                <input type="hidden" id="msg-type" value="">
-                <div class="right clearfix">
-                    <input type="button" id="btn-msg" value="Ok">
-                    <input type="button" id="btn-msg-x" value="Cancel">
-                </div>
-            </div>
-        </div>
-    </div> -->
 
-    <section>
-        <div id="bootstrap-touch-slider" class="carousel bs-slider fade  control-round indicators-line" data-ride="carousel" data-pause="hover" data-interval="5000">
+    <div class="item">
+      <img src="carousel/img2.jpeg" alt="img2" style="width:100%">
+    </div>
 
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <li data-target="#bootstrap-touch-slider" data-slide-to="0" class="active"></li>
-                    <li data-target="#bootstrap-touch-slider" data-slide-to="1"></li>
-                    <li data-target="#bootstrap-touch-slider" data-slide-to="2"></li>
-                </ol>
+    <div class="item">
+      <img src="carousel/img3.jpeg" alt="img3" style="width:100%">
+    </div>
+  </div>
 
-                <!-- Wrapper For Slides -->
-                <div class="carousel-inner" role="listbox">
-
-                    <!-- Third Slide -->
-                    <div class="item active">
-
-                        <!-- Slide Background -->
-                        <img src="https://images.pexels.com/photos/8434/holiday-trip-packaging-8434.jpg?w=1260&h=750&auto=compress&cs=tinysrgb" alt="Bootstrap Touch Slider"  class="slide-image"/>
-                        <div class="bs-slider-overlay"></div>
-
-                        <div class="container">
-                            <div class="row">
-                                <!-- Slide Text Layer -->
-                                <div class="slide-text slide_style_left">
-                                    <h1 class="animated zoomInRight"></h1>
-                                    <p class="animated fadeInLeft"></p>
-                                    <a href="#" class="btn btn-default" class="animated fadeInLeft">Shop Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- End of Slide -->
-                    <!-- Second Slide -->
-                    <div class="item">
-                        <!-- Slide Background -->
-                        <img src="https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" alt="Bootstrap Touch Slider"  class="slide-image"/>
-                        <div class="bs-slider-overlay"></div>
-                        <!-- Slide Text Layer -->
-                        <div class="slide-text slide_style_center">
-                            <h1 class="animated flipInX"></h1>
-                            <p class="animated lightSpeedIn"></p>
-                            <a href="http://bootstrapthemes.co/" target="_blank" class="btn btn-default" class="animated fadeInUp">Shop Now</a>
-                        </div>
-                    </div>
-                    <!-- End of Slide -->
-
-                    <!-- Third Slide -->
-                    <div class="item">
-
-                        <!-- Slide Background -->
-                        <img src="https://images.pexels.com/photos/265620/pexels-photo-265620.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb" alt="Bootstrap Touch Slider"  class="slide-image"/>
-                        <div class="bs-slider-overlay"></div>
-                        <!-- Slide Text Layer -->
-                        <div class="slide-text slide_style_right">
-                            <h1 class="animated zoomInLeft"></h1>
-                            <p class="animated fadeInRight"></p>
-                            <a href="http://bootstrapthemes.co/" target="_blank" class="btn btn-default" class="animated fadeInLeft">Shop Now</a>
-                        </div>
-                    </div>
-                    <!-- End of Slide -->
-
-
-                </div><!-- End of Wrapper For Slides -->
-
-                <!-- Left Control -->
-                <a class="left carousel-control" href="#bootstrap-touch-slider" role="button" data-slide="prev">
-                    <span class="fa fa-angle-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-
-                <!-- Right Control -->
-                <a class="right carousel-control" href="#bootstrap-touch-slider" role="button" data-slide="next">
-                    <span class="fa fa-angle-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-
-            </div> <!-- End  bootstrap-touch-slider Slider -->
-    </section>
-<!-- <section class="sec-search container" style="margin-top: 10px;">
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<section class="sec-search" style="margin-top: 10px;" style="width:100%">
     <form class="form-standard form-wrapper cf" action="" method="post">
     <div class="col-lg-1 col-sm-1 col-md-1 col-xs-1"></div>
     <div class="col-of-search col-lg-5 col-md-9 col-sm-9 col-xs-7 has-feedback">
@@ -131,14 +63,15 @@
     </div>
     <div class="col-xs-1"></div>
     </form>
-</section> -->
-
-<section class="cat-section container">
+</section>
+<br>
+<br>
+      <section>
           <div class="row">
               <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="" class="cat-wrapper thumbnail">
                       <div class="cat-holder">
-                      <img class="cat-img center-block img-responsive" src="resources/includes/images/realestate.png">
+                      <img class="cat-img center-block img-responsive" src=" images/realestate.png">
                       <p class="cat-des text-center">Real Estate</p>
                       </div>
                   </a>
@@ -146,7 +79,7 @@
                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="home/view_product/1/5') " class="cat-wrapper thumbnail">
                       <div class="cat-holder">
-                      <img class="cat-img center-block img-responsive" src="resources/includes/images/clothes.png">
+                      <img class="cat-img center-block img-responsive" src=" images/clothes.png">
                       <p class="cat-des text-center">Clothing and Accessories</p>
                      </div>
                   </a>
@@ -154,7 +87,7 @@
                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="home/view_product/1/11') " class="cat-wrapper thumbnail">
                       <div class="cat-holder">
-                      <img class="cat-img center-block img-responsive" src="resources/includes/images/shoes.png">
+                      <img class="cat-img center-block img-responsive" src=" images/shoes.png">
                       <p class="cat-des text-center">Shoes and Footwear</p>
                       </div>
                   </a>
@@ -162,7 +95,7 @@
               <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="home/view_product/1/2') " class="cat-wrapper thumbnail">
                       <div class="cat-holder">
-                      <img class="cat-img center-block img-responsive" src="resources/includes/images/beautyproducts.png">
+                      <img class="cat-img center-block img-responsive" src=" images/beautyproducts.png">
                       <p class="cat-des text-center">Health and Beauty</p>
                       </div>
                   </a>
@@ -172,7 +105,7 @@
               <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="home/view_product/1/12') " class="cat-wrapper thumbnail">
                       <div class="cat-holder">
-                      <img class="cat-img1 center-block img-responsive" src="resources/includes/images/mobile.png">
+                      <img class="cat-img1 center-block img-responsive" src=" images/mobile.png">
                       <p class="cat-des text-center">Mobiles and Tablets</p>
                       </div>
                   </a>
@@ -180,7 +113,7 @@
                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="home/view_product/1/1') " class="cat-wrapper thumbnail">
                       <div class="cat-holder">
-                      <img class="cat-img center-block img-responsive" src="resources/includes/images/laptop.png">
+                      <img class="cat-img center-block img-responsive" src=" images/laptop.png">
                       <p class="cat-des text-center">Computers and Laptops</p>
                      </div>
                   </a>
@@ -188,7 +121,7 @@
                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="home/view_product/1/9') " class="cat-wrapper thumbnail">
                       <div class="cat-holder">
-                      <img class="cat-img center-block img-responsive" src="resources/includes/images/appliancesandfur.png">
+                      <img class="cat-img center-block img-responsive" src=" images/appliancesandfur.png">
                       <p class="cat-des text-center">Appliances and Furniture</p>
                       </div>
                   </a>
@@ -196,7 +129,7 @@
               <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="home/view_product/1/8') " class="cat-wrapper thumbnail">
                       <div class="cat-holder" style="margin-top: 32.5px;">
-                      <img class="cat-img center-block img-responsive" src="resources/includes/images/automobile.png">
+                      <img class="cat-img center-block img-responsive" src=" images/automobile.png">
                       <p class="cat-des text-center">Automobiles and Motorcycles</p>
                       </div>
                   </a>
@@ -206,7 +139,7 @@
               <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="home/view_product/1/7') " class="cat-wrapper thumbnail">
                       <div class="cat-holder">
-                      <img class="cat-img center-block img-responsive" src="resources/includes/images/foods.png">
+                      <img class="cat-img center-block img-responsive" src=" images/foods.png">
                       <p class="cat-des text-center">Food and Beverages</p>
                       </div>
                   </a>
@@ -214,7 +147,7 @@
                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="home/view_product/1/6') " class="cat-wrapper thumbnail">
                       <div class="cat-holder">
-                      <img class="cat-img center-block img-responsive" src="resources/includes/images/pets.png">
+                      <img class="cat-img center-block img-responsive" src=" images/pets.png">
                       <p class="cat-des text-center">Pets</p>
                      </div>
                   </a>
@@ -222,7 +155,7 @@
                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="home/view_product/1/3') " class="cat-wrapper thumbnail">
                       <div class="cat-holder">
-                      <img class="cat-img center-block img-responsive" src="resources/includes/images/services.png">
+                      <img class="cat-img center-block img-responsive" src=" images/services.png">
                       <p class="cat-des text-center">Services</p>
                       </div>
                   </a>
@@ -230,7 +163,7 @@
               <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
                   <a href="home/view_product/1/10') " class="cat-wrapper thumbnail">
                       <div class="cat-holder">
-                      <img class="cat-img center-block img-responsive" src="resources/includes/images/babystuff.png">
+                      <img class="cat-img center-block img-responsive" src=" images/babystuff.png">
                       <p class="cat-des text-center">Baby Stuff</p>
                       </div>
                   </a>
@@ -326,4 +259,5 @@
       		</div>
       		</div>
       	</div>
-      <?php include 'footer.php' ?>
+</body>
+</html>
--- plugins.php
+++ plugins.php
@@ -1,31 +1,34 @@
-<link rel="Shortcut Icon" type="image/ico" href="resources/includes/img/favicon.ico">
-
 <!-- LOCAL CSS-->
-<link rel="stylesheet" href="resources/includes/css/bootstrap.min.css">
-<link rel="stylesheet" href="resources/includes/css/main.css">
-<link rel="stylesheet" href="resources/includes/css/aboutetc.css">
-<link rel="stylesheet" href="resources/includes/css/accountcat.css">
-<link rel="stylesheet" href="resources/includes/css/animate.css">
-<link rel="stylesheet" href="resources/includes/css/login.css">
-<link rel="stylesheet" href="resources/includes/css/productview.css">
-<link rel="stylesheet" href="resources/includes/js/vendor/jquery-ui/jquery-ui.css">
-<link rel="stylesheet" href="resources/includes/js/vendor/jquery-ui/jquery-ui.theme.css">
-<link rel="stylesheet" href="resources/includes/css/jhtml/jHtmlArea.css">
-<link rel="stylesheet" href="resources/includes/css/jhtml/jqueryui/ui-lightness/jquery-ui-1.7.2.custom.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/aboutetc.css">
+<link rel="stylesheet" href="css/accountcat.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/login.css">
+<link rel="stylesheet" href="css/productview.css">
+<link rel="stylesheet" href="js/vendor/jquery-ui/jquery-ui.css">
+<link rel="stylesheet" href="js/vendor/jquery-ui/jquery-ui.theme.css">
+<link rel="stylesheet" href="css/jhtml/jHtmlArea.css">
+<link rel="stylesheet" href="css/jhtml/jqueryui/ui-lightness/jquery-ui-1.7.2.custom.css">
   <!-- Bootstrap Core CSS -->
 
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <!-- MetisMenu CSS -->
+    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="https://cdn.bootcss.com/startbootstrap-sb-admin-2/3.3.7+1/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="dist/css/sb-admin-2.min.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
+    <!-- Multi select-->
+    <link href="dist/css/bootstrap-multiselect.css" rel="stylesheet" type="text/css">
     <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="vendor/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
     <!--Google Fonts-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
@@ -35,22 +38,15 @@
      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <!-- End of Font Awesome CDN-->
     <!-- JAVASCRIPT -->
-    <script type="text/javascript" src="resources/includes/js/bootstrap-modal-animate-css.js"></script>
+    <script type="text/javascript" src="js/bootstrap-modal-animate-css.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!--End of JAVA SCRIPT -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-      (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-5671596422665933",
-        enable_page_level_ads: true
-      });
-    </script>
-
 <?php
+session_start();
 function getConnection(){
     $servername = "127.0.0.1"; //servername
     $username = "root"; //username
